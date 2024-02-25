a0a0103d111aea7abe246fecf1137013be83e879
forNerzul <sbeardman92@gmail.com>
2024-02-25 16:17:15 -0300

    agregado archivo script para inicializar el sv en linux/mac

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+java -Xmx5120M -Xms5120M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-1.20.1-196.jar
+
+# generado con: https://docs.papermc.io/misc/tools/start-script-gen
